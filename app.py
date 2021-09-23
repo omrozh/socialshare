ad17e80d85eb6f5004d90bc59fc51dd0b9a40483
omrozh <omrozh@hotmail.com>
2021-09-23 23:23:02 +0300

    inti

---
+++ app.py
@@ -0,0 +1,69 @@
+import flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+
+app = flask.Flask(__name__)
+
+app.config["SECRET_KEY"] = "InfinityCorporation"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://qftmofzjbfryth:cf04f93c34d0c36a68c991637ef24f0247bc3cb" \
+                                        "92e5655d863421712b47cd0c3@ec2-54-195-246-55.eu-west-1.compute.amazonaws.com" \
+                                        ":5432/d3hk4ichdip6ol"
+
+db = SQLAlchemy(app)
+CORS(app, support_credentials=True)
+
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    score = db.Column(db.Integer)
+    category = db.Column(db.String)
+
+
+@app.route("/saveScore", methods=["POST", "GET"])
+@cross_origin(supports_credentials=True)
+def saveScore():
+    if flask.request.method == "POST":
+        values = flask.request.values
+        if values["category"] == "hard":
+            new_score = Score(name=values["name"], score=int(values["score"]) * 3, category=values["category"])
+        elif values["category"] == "extreme":
+            new_score = Score(name=values["name"], score=int(values["score"]) * 5, category=values["category"])
+        else:
+            new_score = Score(name=values["name"], score=int(values["score"]), category=values["category"])
+        db.session.add(new_score)
+        db.session.commit()
+        return "Completed"
+
+
+@app.route("/viewScores", methods=["GET"])
+def viewScores():
+    all_scores = []
+
+    for i in Score.query.all():
+        all_scores.append(i.score)
+
+    top_ten_scores = sorted(all_scores)[-10:]
+
+    final_top_ten = []
+
+    for i in top_ten_scores:
+        final_top_ten.append(Score.query.filter_by(score=int(i)).first())
+
+    final_top_ten.reverse()
+    final_dict_list = []
+
+    for i in final_top_ten:
+        final_dict_list.append({"name": i.name, "score": str(i.score), "category": i.category})
+    return flask.render_template("main.html", final_top_ten=final_dict_list)
+
+
+@app.route("/")
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route("/<filename>")
+def appleReturn(filename):
+    return flask.send_file("data/" + filename)
+
---
+++ data/index.js
@@ -0,0 +1,193 @@
+
+var difficulty = 3;
+var send_dif = 3;
+
+function setDif(index_dif, dif_button){
+    for(i = 0; i < document.getElementsByClassName("mainFormBig").length; i++){
+        document.getElementsByClassName("mainFormBig")[i].style.backgroundColor = "white";
+        document.getElementsByClassName("mainFormBig")[i].style.color = "gray";
+    }
+    difficulty = index_dif;
+    localStorage.setItem("lastDif", index_dif)
+    dif_button.style.backgroundColor = "#4CAF50";
+    if(send_dif == 3){
+        send_dif = difficulty;
+    }
+    dif_button.style.color = "white";
+}
+
+function setName(){
+    if(localStorage.getItem("playerName")){
+        document.getElementById("Name").value = localStorage.getItem("playerName");
+    }
+}
+function setNameNew(){
+    localStorage.setItem("playerName", document.getElementById("Name").value)
+}
+
+function getLeaderBoard(){
+    setName()
+    fetch('/viewScores').then(function (response) {
+        return response.text();
+    }).then(function (html) {
+        document.getElementsByClassName("textBoxLead")[0].innerHTML += html;
+    }).catch(function (err) {
+        console.warn('Something went wrong.', err);
+    });
+}
+
+function main(){
+    getLeaderBoard()
+    if(localStorage.getItem("lastDif")){
+        difficulty = parseInt(localStorage.getItem("lastDif"));
+    }
+    var player = document.getElementsByClassName("player");
+    var x_pos = 288;
+    var y_pos = 195;
+    var current_direction = 4;
+    var prev_direction = 0;
+    var food = document.getElementById("food");
+    var point = 1;
+    var end_game = false;
+
+    var container = document.getElementById("mainContainer")
+    var containerHeight = parseInt(container.style.height.replace("px", ""));
+    var containerWidth = parseInt(container.style.width.replace("px", ""));
+
+    spawn_food();
+    function game_over(){
+        end_game = true;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", '/saveScore', true)
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        var dict_levels = {
+            1: "easy",
+            3: "medium",
+            4: "hard",
+            5: "extreme"
+        }
+            xhr.send("name=" + document.getElementById("Name").value + "&score=" + point + "&category=" + dict_levels[send_dif])
+
+        document.getElementById("game_over_text").style.visibility = "visible";
+        return;
+    }
+    function spawn_new(){
+        player[player.length - 1].insertAdjacentHTML("afterend", "<div class='player'></div>");
+        player = document.getElementsByClassName("player");
+    }
+    function check_collision(x_pos, y_pos){
+        if(x_pos + 50 > containerWidth){
+            game_over()
+        }
+        if(x_pos < 0){
+            game_over()
+        }
+        if(y_pos < 0){
+            game_over()
+        }
+        if(y_pos + 50 > containerHeight){
+            game_over()
+        }
+        var x_food_pos = parseInt(food.style.left.replace("px", ""));
+        var y_food_pos = parseInt(food.style.top.replace("px", ""));
+
+        if(x_pos + 50 > x_food_pos && x_pos < x_food_pos + 50 && y_pos + 50 > y_food_pos && y_pos < y_food_pos + 50){
+            point += 1;
+            spawn_new();
+            spawn_food()
+        }
+    }
+
+    function spawn_food(){
+        x_cor = Math.floor(Math.random() *  containerWidth - 50) - 100;
+        y_cor = Math.floor(Math.random() *  containerHeight - 50) - 100;
+
+        food.style.left = x_cor + 100 + "px";
+        food.style.top = y_cor + 100 + "px";
+
+    }
+    
+
+    function input_direction(e) {
+        if(e.code == "ArrowDown" && current_direction != 3){
+            current_direction = 1;
+        }
+        else if(e.code == "ArrowRight" && current_direction != 2){
+            current_direction = 0;
+        }
+        else if(e.code == "ArrowLeft" && current_direction != 0){
+            current_direction = 2;
+        }
+        else if(e.code == "ArrowUp" && current_direction != 1){
+            current_direction = 3;
+        }
+        else if(e.code == "KeyE"){
+            spawn_food();
+        }
+        else if(e.code == "Enter"){
+            window.location.reload()
+        }
+        else if(e.code == "Space"){
+            if(current_direction != 4){
+                prev_direction = current_direction;
+                current_direction = 4;
+            }
+            else if(current_direction == 5){
+                alert("Zorluk Seç")
+            }
+            else{
+                current_direction = prev_direction;
+            }
+        }
+    }
+    document.addEventListener('keydown', input_direction);
+
+    function update_pos(player_index, x_pos, y_pos, player){
+        if(current_direction == 0){
+            player[player_index].style.left = x_pos - (5 * player_index) + "px";
+            player[player_index].style.top = y_pos + "px";
+        }
+        else if(current_direction == 2){
+            player[player_index].style.left = x_pos + (5 * player_index) + "px";
+            player[player_index].style.top = y_pos + "px";
+        }
+        else{
+            player[player_index].style.left = x_pos + "px";
+            player[player_index].style.top = y_pos + (5 * player_index) + "px";
+        }
+    }
+
+    function move(){
+        if(end_game){
+            return;
+        }
+        player = document.getElementsByClassName("player");
+        
+        document.getElementById("point_counter").innerHTML = "Point: " + point
+        if(current_direction == 0){
+            x_pos += point / 10 + difficulty;
+        }
+        if(current_direction == 1){
+            y_pos += point / 10 + difficulty;
+        }
+        if(current_direction == 2){
+            x_pos -= point / 10 + difficulty;
+        }
+        if(current_direction == 3){
+            y_pos -= point / 10 + difficulty;
+        }
+
+        for(i = 0; i < player.length; i++){
+            update_pos(i, x_pos, y_pos, player);
+        }
+
+        check_collision(x_pos, y_pos)
+        setTimeout(move, 1);
+    }
+    setTimeout(move, 20);
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    main();
+});
