7fbe538eaba76b28db98e416ec23e0fb70128797
omrozh <omrozh@hotmail.com>
2021-09-28 20:37:15 +0300

    ban

--- data/index.js
+++ data/index.js
@@ -247,20 +247,20 @@ function main(){
 
     function update_pos(player_index, x_pos, y_pos, player){
         if(current_direction == 0){
-            player[player_index].style.left = x_pos + (5 * player_index) + "px";
+            player[player_index].style.left = x_pos + (2.5 * player_index) + "px";
             player[player_index].style.top = y_pos + "px";
         }
         else if(current_direction == 2){
-            player[player_index].style.left = x_pos - (5 * player_index) + "px";
+            player[player_index].style.left = x_pos - (2.5 * player_index) + "px";
             player[player_index].style.top = y_pos + "px";
         }
         else if(current_direction == 3){
             player[player_index].style.left = x_pos + "px";
-            player[player_index].style.top = y_pos - (5 * player_index) + "px";
+            player[player_index].style.top = y_pos - (2.5 * player_index) + "px";
         }
         else if(current_direction == 1){
             player[player_index].style.left = x_pos + "px";
-            player[player_index].style.top = y_pos + (5 * player_index) + "px";
+            player[player_index].style.top = y_pos + (2.5 * player_index) + "px";
         }
         else{
             player[player_index].style.left = x_pos + "px";
