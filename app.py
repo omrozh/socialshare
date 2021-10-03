b19e64bfdbedb82bcc7b9002ecc419683a7bcbe7
omrozh <omrozh@hotmail.com>
2021-10-03 18:28:41 +0300

    snake ads

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ def viewScores():
     for i in all_objects:
         all_scores.append(i.score)
 
-    top_ten_scores = Score.query.order_by(Score.score).limit(10).all()
+    top_ten_scores = Score.query.order_by(Score.score).all()
     top_ten_scores.reverse()
-    final_top_ten = top_ten_scores
+    final_top_ten = top_ten_scores[:10]
 
     final_dict_list = []
 
