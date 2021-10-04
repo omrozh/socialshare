a34839f89aba454974fccea92dda71bf7049c83c
omrozh <omrozh@hotmail.com>
2021-10-04 21:17:13 +0300

    social update

--- data/index.js
+++ data/index.js
@@ -90,6 +90,26 @@ function mobileUse(){
 function main(){
     getLeaderBoard()
     mobileUse()
+    var modal = document.getElementById("myModal");
+
+    var btn = document.getElementById("myBtn");
+
+    var span = document.getElementsByClassName("close")[0];
+
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
+
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
+
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
+
     if(localStorage.getItem("lastDif")){
         difficulty = 6;
     }
