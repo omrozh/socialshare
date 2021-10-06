a8d38f1d47dabb13b574ad76a60311e24f178774
omrozh <omrozh@hotmail.com>
2021-10-06 10:09:06 +0300

    back

--- data/index.js
+++ data/index.js
@@ -87,6 +87,13 @@ function mobileUse(){
     }
 }
 
+function() openModal{
+  alert("Work")
+  var modal = document.getElementById("myModal");
+  modal.innerHTML = '<div class="modal-content"> <span class="close" style="opacity: 0">&times;</span> <iframe src="https://sosyalke.herokuapp.com/mainPage/no_cat/social_snake" style="width: 100%; height: 100%;" frameborder="0"></iframe> </div>'
+  modal.style.display = "block";
+}
+
 function main(){
     getLeaderBoard()
     mobileUse()
