4aecd8135524db3c51a8589ab210e333ca83cc89
omrozh <omrozh@hotmail.com>
2021-10-06 10:14:37 +0300

    back

--- data/index.js
+++ data/index.js
@@ -101,7 +101,6 @@ function main(){
     }
 
     btn.ontouchstart = function() {
-      alert("Work")
       modal.innerHTML = '<div class="modal-content"> <span class="close" style="opacity: 0">&times;</span> <iframe src="https://sosyalke.herokuapp.com/mainPage/no_cat/social_snake" style="width: 100%; height: 100%;" frameborder="0"></iframe> </div>'
       modal.style.display = "block";
     }
